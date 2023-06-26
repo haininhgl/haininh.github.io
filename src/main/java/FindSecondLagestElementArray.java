3b2201687395fe9b786434ac56788e1efe5d3b24
haininhgl <haininhgl@gmail.com>
2023-06-26 14:44:53 +0700

    adding files

---
+++ src/main/java/FindSecondLagestElementArray.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class FindSecondLagestElementArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số lượng phần tử trong mảng: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Nhập các phần tử của mảng:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("Không tồn tại phần tử lớn thứ hai trong mảng.");
+        } else {
+            System.out.println("Phần tử lớn thứ hai trong mảng là: " + secondLargest);
+        }
+    }
+}
---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for (int i = 1; i <= a; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
